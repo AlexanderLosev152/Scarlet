5693f8fee2b0041770beb554e06c8666bfdfa304
Alexander <losev.pavlovo@mail.ru>
2023-05-12 18:03:17 +0300

    go

--- js/main.js
+++ js/main.js
@@ -4,3 +4,29 @@ let menu = document.querySelector(".menu__icon");
 menu.onclick = () => {
   menu.classList.toggle("move");
 };
+
+// slider
+var swiper = new Swiper(".services__content", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 25000,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// header
+
+let header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+  header.classList.toggle("shadow", window.scrollY > 0);
+});
